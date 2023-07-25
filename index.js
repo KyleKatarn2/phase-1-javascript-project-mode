9a0d8ba458860965e8691a6317109f0fb0d9361b
Jonathan Campos <Malvyx5@gmail.com>
2023-07-25 19:21:47 -0400

    Added a form and list elements to the html

---
+++ index.js
@@ -0,0 +1,5 @@
+const init = () => {
+    const form = document.querySelector("#Ace-form");
+    const list = document.querySelector("#aircraft-list");
+    const info = document.querySelector("#info-list");
+};
