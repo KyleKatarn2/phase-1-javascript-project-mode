4d7f7225a0312af5564f3fd4e11fa2f3e7baa58a
Jonathan Campos <Malvyx5@gmail.com>
2023-07-26 14:53:26 -0400

    Added form event listener

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const init = () => {
     const form = document.querySelector("#Ace-form");
     const list = document.querySelector("#aircraft-list");
     const info = document.querySelector("#info-list");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input#search");
+        list.innerHTML = "";
+        info.innerHTML = "";
+    });
 };
